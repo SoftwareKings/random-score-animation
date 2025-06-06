f514b07365e69c7f0e73a1136be8be5f44b8a2a7
SoftwareKings <dragonfighter0120@gmail.com>
2025-06-06 06:42:00 -0400

    verify token on frontend

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import { jwtDecode } from 'jwt-decode';
 import type {
   RouteRecordRaw,
   RouteLocationNormalized,
@@ -25,10 +26,24 @@ router.beforeEach((
   from: RouteLocationNormalized,
   next: NavigationGuardNext
 ) => {
-  if (to.meta.requiresAuth && !localStorage.getItem('token')) {
-    return next({ name: 'Login' })
+  if (!to.meta.requiresAuth) return next();
+
+  const token = localStorage.getItem('token');
+  if (!token) return next({ name: 'Login' });
+
+  try {
+    const decodedToken = jwtDecode(token);
+    const currentTime = Date.now() / 1000;
+
+    if (!decodedToken || (decodedToken.exp && decodedToken.exp < currentTime)) {
+      return next({ name: 'Login' });
+    }
+
+    return next();
+  } catch (err) {
+    return next({ name: 'Login' }); // Token decoding failed
   }
-  next()
+
 })
 
 export default router
